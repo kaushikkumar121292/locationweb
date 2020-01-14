c42275318c4e5e2d379a1f3562e7dca221387dfd
kaushikkumarsinha <kaushikkumarsinha@10.255.55.33>
2020-01-14 07:53:39 +0530

    14/01/2020

--- locationweb/src/main/java/com/bharath/location/LocationwebApplication.java
+++ locationweb/src/main/java/com/bharath/location/LocationwebApplication.java
@@ -2,9 +2,18 @@ package com.bharath.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LocationwebApplication {
+public class LocationwebApplication extends SpringBootServletInitializer {
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(LocationwebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationwebApplication.class, args);
